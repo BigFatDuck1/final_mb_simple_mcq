707ee5953fc204dd736372bf7d3a6f8ad2b5cd9f
BigFatDuck1 <clementho999@gmail.com>
2024-12-02 23:50:43 +0800

    Initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='overall_box'>
+      <div className="header">
+        Header
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="main_container">
+        <div className="question_panel">
+          Mock Question
+        </div>
+        <div className="select_panel">
+          Select question
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
