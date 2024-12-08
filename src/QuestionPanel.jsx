6cb38604186568dc28472ad7e053110b1f4f429a
Clement <clementho999@gmail.com>
2024-12-09 00:39:37 +0800

    Reformatted <li> and added checkbox (incomplete functionality)

--- src/QuestionPanel.jsx
+++ src/QuestionPanel.jsx
@@ -7,37 +7,35 @@ function QuestionPanel({ question, a,b,c,d,e, correct, explanation, showAnswer }
 
     // const [showAnswer, setShowAnswer] = useState(false);
 
-    if (showAnswer == false) {
-        return (
-            <div>
-                <h2>{question}</h2>
-                <ol type="A">
-                    <li>{a}</li>
-                    <li>{b}</li>
-                    <li>{c}</li>
-                    <li>{d}</li>
-                    <li>{e}</li>
-                </ol>
-            </div>
-        )
-    }
-    else { //showAnswer == true
-        return (
-            <div>
-                <h2>{question}</h2>
-                <ol type="A">
-                    <li>{a}</li>
-                    <li>{b}</li>
-                    <li>{c}</li>
-                    <li>{d}</li>
-                    <li>{e}</li>
-                </ol>
+    let list_of_options = [a,b,c,d,e];
+    return (
+        <div>
+            <h2>{question}</h2>
+            <ol type="A">
+                {
+                    list_of_options.map((item) => {
+                        let i = 0;
+                        let new_option =  
+                        <li key={i} >
+                            <input type="checkbox" name="checkbox" />
+                            {item}
+                        </li>; //TODO: add key properly
+                        return new_option;
+                    })
+                }
+            </ol>
+            {
+                showAnswer ? 
+                <div>
                 <h5>Correct Answer: {correct}</h5>
-                <p>{explanation}</p>
-            </div>
-        )
-    }
-
+                <p>{explanation}</p> 
+                </div>
+                : 
+                <div></div>
+                
+            }
+        </div>
+    )
 }
 
 export default QuestionPanel;
