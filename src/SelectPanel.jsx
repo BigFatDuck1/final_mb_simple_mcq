53f09978798fd758a72b308e879f20e7d93adc25
Clement <clementho999@gmail.com>
2024-12-25 14:39:51 +0800

    Displays question previews on side panel

--- src/SelectPanel.jsx
+++ src/SelectPanel.jsx
@@ -1,9 +1,29 @@
 //import React from "react";
+import questions from "./questions.json";
+
 
 function SelectPanel() {
+    let questions_array = []; //Subject + Topic
+    for (const id in questions) {
+        questions_array.push(questions[id]);
+    }
+    console.log(questions_array);
+
     return (
         <div>
-            <h2>Select question</h2>
+            <h3>Select question</h3>
+            <ol>
+                {
+                    questions_array.map((item) => {
+                        let q_preview = 
+                        <li>
+                            <h4>{item.subject}</h4>
+                            <p>{item.topic}</p>
+                        </li>
+                        return q_preview;
+                    })
+                }
+            </ol>
         </div>
     )
 }
